86f985552efd85cf8e350abef46514eb1ae97082
papifear <elvis.simanis@gmail.com>
2023-10-06 15:40:00 +0300

    Encryption in transit

--- client/websockets_client.gd
+++ client/websockets_client.gd
@@ -11,17 +11,17 @@ signal error
 var _client = WebSocketPeer.new()
 
 func _ready():
-	###_client.connect("connection_closed", self, "_closed")
-	###_client.connect("connection_error", self, "_closed")
-	###_client.connect("connection_established", self, "_connected")
-	###_client.connect("data_received", self, "_on_data")
+	_client.connect("connection_closed", _closed)
+	_client.connect("connection_error", _closed)
+	#_client.connect("connection_established", _connected)
+	#_client.connect("data_received", _on_data)
 	pass
 	
 func connect_to_server(hostname: String, port: int) -> void:
 	# Connects to the server or emits an error signal
 	# If connected, emit a connect signal
-	var websocket_url = "ws://%s:%d" % [hostname, port]
-	var err = _client.connect_to_url(websocket_url)
+	var websocket_url = "wss://%s:%d" % [hostname, port]
+	var err = _client.connect_to_url(websocket_url, TLSOptions.client_unsafe())
 	if err:
 		print("Unable to connect")
 		set_process(false)
@@ -32,10 +32,10 @@ func send_packet(packet: Packet) -> void:
 	# Sends a packet to the server
 	_send_string(packet.tostring())
 	
-#func _closed(was_clean = false):
-#	print("Closed, clean: ", was_clean)
-#	set_process(false)
-#	emit_signal("disconnected", was_clean)
+func _closed(was_clean = false):
+	print("Closed, clean: ", was_clean)
+	set_process(false)
+	emit_signal("disconnected", was_clean)
 	
 func _connected(proto = ""):
 	print("Connected with protocol: ", proto)
@@ -43,7 +43,7 @@ func _connected(proto = ""):
 	
 func _on_data():
 	var data: String = _client.get_packet().get_string_from_utf8()
-	print("Got data from server: ", data)
+	#print("Got data from server: ", data)
 	emit_signal("data", data)
 	
 func _process(delta):
@@ -51,15 +51,15 @@ func _process(delta):
 	var state = _client.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
 		while _client.get_available_packet_count():
-			#print("Packet: ", _client.get_packet())
 			_on_data()
 	elif state == WebSocketPeer.STATE_CLOSING:
 		# Keep polling to achieve proper close.
 		pass
 	elif state == WebSocketPeer.STATE_CLOSED:
-		var code = _client.get_close_code()
-		var reason = _client.get_close_reason()
-		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		#var code = _client.get_close_code()
+		#var reason = _client.get_close_reason()
+		#print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		#emit_signal("disconnected", code != -1)
 		set_process(false) # Stop processing.
 	
 func _send_string(string: String) -> void:
--- server/__main__.py
+++ server/__main__.py
@@ -2,13 +2,13 @@ import manage
 import sys
 import protocol
 from twisted.python import log
-from twisted.internet import reactor, task
+from twisted.internet import reactor, task, ssl
 from autobahn.twisted.websocket import WebSocketServerFactory
 
 class GameFactory(WebSocketServerFactory):
     def __init__(self, hostname: str, port: int):
         self.protocol = protocol.GameServerProtocol
-        super().__init__(f'ws://{hostname}:{port}')
+        super().__init__(f'wss://{hostname}:{port}')
 
         self.players: set[protocol.GameServerProtocol] = set()
         self.tickrate: int = 20 # 20 times per second
@@ -29,8 +29,11 @@ class GameFactory(WebSocketServerFactory):
 if __name__ == '__main__':
     log.startLogging(sys.stdout)
 
+    certs_dir: str = f"{sys.path[0]}/certs/"
+    contextFactory = ssl.DefaultOpenSSLContextFactory(certs_dir + "server.key", certs_dir + "server.crt")
+
     PORT: int = 8081
     factory = GameFactory('0.0.0.0', PORT)
 
-    reactor.listenTCP(PORT, factory)
+    reactor.listenSSL(PORT, factory, contextFactory)
     reactor.run()
